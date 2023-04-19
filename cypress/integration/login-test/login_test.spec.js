bbc88dee74f7b4d2c4f6a73fdc663ca4fd3e9110
Julia Rodrigues <juliarodrigues606@gmail.com>
2023-04-19 16:34:49 -0300

    incluindo cenarios de falha

--- cypress/integration/login-test/login_test.spec.js
+++ cypress/integration/login-test/login_test.spec.js
@@ -26,7 +26,7 @@ describe('Suíte de teste para realizar login', () => {
       cy.get(loginElements.msnError).should('have.text','Epic sadface: Username and password do not match any user in this service')
     })
 
-    it.only('Não deve realizar login ao enviar username incorreto', () => {
+    it('Não deve realizar login ao enviar username incorreto', () => {
       cy.get(loginElements.inputUsername).should('have.attr','placeholder','Username')
       cy.get(loginElements.inputUsername).type('user.username')
       cy.get(loginElements.inputPassword).should('have.attr','placeholder','Password')
@@ -34,5 +34,32 @@ describe('Suíte de teste para realizar login', () => {
       cy.get(loginElements.btnLogin).should('have.value','Login')
       cy.get(loginElements.btnLogin).click()
       cy.get(loginElements.msnError).should('have.text','Epic sadface: Username and password do not match any user in this service')
+    })
+
+    it('Deve fechar mensagem de erro do login', () => {
+      cy.get(loginElements.inputUsername).should('have.attr','placeholder','Username')
+      cy.get(loginElements.inputUsername).type('user.username')
+      cy.get(loginElements.inputPassword).should('have.attr','placeholder','Password')
+      cy.get(loginElements.inputPassword).type(user.password)
+      cy.get(loginElements.btnLogin).should('have.value','Login')
+      cy.get(loginElements.btnLogin).click()
+      cy.get(loginElements.msnError).should('have.text','Epic sadface: Username and password do not match any user in this service')
+      cy.get(loginElements.btnFecharMensagemErro).click()
+    })
+
+    it('Não deve realizar login ao não enviar username', () => {
+      cy.get(loginElements.inputPassword).should('have.attr','placeholder','Password')
+      cy.get(loginElements.inputPassword).type(user.password)
+      cy.get(loginElements.btnLogin).should('have.value','Login')
+      cy.get(loginElements.btnLogin).click()
+      cy.get(loginElements.msnError).should('have.text','Epic sadface: Username is required')
+    })
+
+    it('Não deve realizar login ao não enviar username', () => {
+      cy.get(loginElements.inputPassword).should('have.attr','placeholder','Password')
+      cy.get(loginElements.inputPassword).type(user.password)
+      cy.get(loginElements.btnLogin).should('have.value','Login')
+      cy.get(loginElements.btnLogin).click()
+      cy.get(loginElements.msnError).should('have.text','Epic sadface: Username is required')
     })
   })
