91fb62228ff9b4e71af9e614af2b24fea3768c69
Julia Rodrigues <juliarodrigues606@gmail.com>
2023-04-19 16:36:24 -0300

    commit

--- cypress/support/resources/pages/login/login_page.js
+++ cypress/support/resources/pages/login/login_page.js
@@ -2,5 +2,6 @@ export const ELEMENTS_LOGIN = {
     inputUsername: '[data-test="username"]',
     inputPassword: '[data-test="password"]',
     btnLogin:'[data-test="login-button"]',
-    msnError: '[data-test="error"]'
+    msnError: '[data-test="error"]',
+    btnFecharMensagemErro: '.error-button'
 }
